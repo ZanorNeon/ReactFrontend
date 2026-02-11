62c478222626de62e38626b33f231e328da8539a
ZanorNeon <raytherayoor@gmail.com>
2026-02-11 18:33:34 +0200

    Some Clean Up.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 
-import ToDos, { loader as ToDosLoader } from './routes/ToDos';
-import NewToDo, { action as newToDoAction } from './routes/NewToDo';
-import PostDetails, { loader as toDoDetailsLoader } from './routes/ToDoDetails';
+import ToDos, {loader as ToDosLoader} from './routes/ToDos';
+import NewToDo, {action as newToDoAction} from './routes/NewToDo';
+import PostDetails, {loader as toDoDetailsLoader} from './routes/ToDoDetails';
 import RootLayout from './routes/RootLayout';
 import './index.css';
+import Footer from "./components/Footer";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <RootLayout />,
+        element: <RootLayout/>,
         children: [
             {
                 path: '/',
-                element: <ToDos />,
+                element: <ToDos/>,
                 loader: ToDosLoader,
                 children: [
-                    { path: '/create-toDo', element: <NewToDo />, action: newToDoAction },
-                    { path: '/:toDoId', element: <PostDetails />, loader: toDoDetailsLoader }
+                    {path: '/create-toDo', element: <NewToDo/>, action: newToDoAction},
+                    {path: '/:toDoId', element: <PostDetails/>, loader: toDoDetailsLoader},
+                    <Footer></Footer>
                 ],
             },
         ],
@@ -28,6 +30,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <RouterProvider router={router}/>
     </React.StrictMode>
 );
