6f2ef9135a932efb69e8fe1688db2fab156c8818
ZanorNeon <raytherayoor@gmail.com>
2026-03-03 19:52:51 +0200

    Delete button added.

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,12 +1,16 @@
 import {Link} from 'react-router-dom';
 
-import classes from '../components-css/ToDo.module.css';
+import toDoClasses from '../components-css/ToDo.module.css';
+import deleteClasses from '../routes-css/ToDoDelete.module.css';
 
 function ToDo({id, text}) {
     return (
-        <li className={classes.toDo}>
+        <li className={toDoClasses.toDo}>
             <Link to={id}>
-                <p className={classes.text}>{text}</p>
+                <p className={toDoClasses.text}>{text}</p>
+            </Link>
+            <Link to={`/delete`} className={deleteClasses.deleteButton}>
+                Delete
             </Link>
         </li>
     );
